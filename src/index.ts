67162d76a2ac76c892aa7668a3d7c66f6a14a9a7
Andres Sanabria <andrs.dev@gmail.com>
2020-05-18 10:33:25 -0600

    finished development configuration

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import * as express from 'express';
 import * as cors from 'cors';
 import * as data from './data.json';
 
+//Data initialization
+const documents: any = data.documents; 
+
+//Api endpoint
 const app = express();
 app.use(cors({ origin: true }));
 
@@ -10,8 +14,21 @@ app.get('/', (request, response) => {
     response.send(data);
 });
 
+//Returns dpi data from its number
 app.get('/dpi', (request, response) => {
-    response.send('dpi endopoint');
+    if(!request.query?.number){
+        response.status(400).send("[number] query parameter required.");
+    }
+
+    const number: any = request.query.number;
+    const document: any = documents[number];
+    if(document){
+        response.send(documents[number]);
+    } else {
+        response.status(400).send("Invalid dpi number asdfs.");
+    }
 });
+
+
 
 export const api = functions.https.onRequest(app);
